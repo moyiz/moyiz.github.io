dfb193cd8f6a67ba66d3b7001381c2ab864c59f8
moyiz <8603313+moyiz@users.noreply.github.com>
2024-12-26 11:53:03 +0200

    fix icon paths

--- js/state.js
+++ js/state.js
@@ -3,10 +3,10 @@ let s = sessionStorage
 function toggleInvert(img) {
   document.body.classList.toggle("invert")
   if (document.body.classList.contains("invert")) {
-    img.src = "_src/icons/sun-bright.svg"
+    img.src = "_icons/sun-bright.svg"
     s.setItem("invert", true)
   } else {
-    img.src = "_src/icons/moon-stars.svg"
+    img.src = "_icons/moon-stars.svg"
     s.removeItem("invert")
   }
 }
@@ -15,10 +15,10 @@ function toggleMenu(img) {
   const menu = document.querySelector(".menu")
   menu.classList.toggle("hide-menu")
   if (menu.classList.contains("hide-menu")) {
-    img.src = "_src/icons/bars.svg"
+    img.src = "_icons/bars.svg"
     s.setItem("menu", true)
   } else {
-    img.src = "_src/icons/list.svg"
+    img.src = "_icons/list.svg"
     s.removeItem("menu")
   }
 }
@@ -27,10 +27,10 @@ function toggleSmaller(img) {
   const layout = document.querySelector(".layout")
   layout.classList.toggle("smaller")
   if (layout.classList.contains("smaller")) {
-    img.src = "_src/icons/arrows-from-line.svg"
+    img.src = "_icons/arrows-from-line.svg"
     s.setItem("smaller", true)
   } else {
-    img.src = "_src/icons/arrows-to-line.svg"
+    img.src = "_icons/arrows-to-line.svg"
     s.removeItem("smaller")
   }
 }
@@ -39,10 +39,10 @@ function toggleNarrower(img) {
   const layout = document.querySelector(".layout")
   layout.classList.toggle("narrower")
   if (layout.classList.contains("narrower")) {
-    img.src = "_src/icons/arrows-left-right-to-line.svg"
+    img.src = "_icons/arrows-left-right-to-line.svg"
     s.setItem("narrower", true)
   } else {
-    img.src = "_src/icons/compress-wide.svg"
+    img.src = "_icons/compress-wide.svg"
     s.removeItem("narrower")
   }
 }
